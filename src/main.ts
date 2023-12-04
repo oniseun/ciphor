de9adfcdda9f88479f8295a6e186135b125eace7
Victor Oni <seunoni34@gmail.com>
2023-12-04 15:22:20 +0000

    Create brolilerplate nestjs for ciphor repo

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './ciphor.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Mrs Wordsmith Account Api')
+    .setDescription('The wordsmith Account API')
+    .setVersion('1.0')
+    .addTag('accounts')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
